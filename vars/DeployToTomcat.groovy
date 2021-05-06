929aa796dc64f80b8bc77dd6f953d2521b0601a9
shobha9395 <72294984+shobha9395@users.noreply.github.com>
2021-05-06 21:46:47 +0530

    Update DeployToTomcat.groovy

--- vars/DeployToTomcat.groovy
+++ vars/DeployToTomcat.groovy
@@ -1,3 +1,9 @@
 def call (ip){
+  sshagent(['Tomcat']) {
+    sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@${ip}:/opt/tomcat8/webapps/durga.war"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/shutdown.sh"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
+    
+  }
   echo "Deploy to tomcat on ip= ${ip}"
 }
