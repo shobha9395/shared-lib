88b7bab0f174751a832424251e1d6c43ece84e9e
shobha9395 <72294984+shobha9395@users.noreply.github.com>
2021-05-06 22:05:08 +0530

    Update DeployToTomcat.groovy

--- vars/DeployToTomcat.groovy
+++ vars/DeployToTomcat.groovy
@@ -1,5 +1,5 @@
 def call (ip,credId){
-  sshagent(['credId']) {
+  sshagent([credId]) {
     sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@${ip}:/opt/tomcat8/webapps/durga.war"
     sh "ssh ec2-user@${ip} /opt/tomcat8/bin/shutdown.sh"
     sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
